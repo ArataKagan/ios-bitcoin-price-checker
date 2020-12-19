7158e3afb530f96c9d1b90d69304d8d51aca9007
Arata Kagan <aratakagan@gmail.com>
2020-12-18 22:23:13 -0800

    debugging url session failed error

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -15,6 +15,24 @@ struct CoinManager {
     
     func getCoinPrice(for currency:String){
         print(currency)
+        let urlString = "https://rest.coinapi.io/v1/exchangerate/BTC/\(currency)?apiKey=944BF66B-9036-4FD6-8FA4-6B1240DF1037"
+        performRequest(with: urlString)
+        
+    }
+    
+    func performRequest(with urlString: String){
+        if let url = URL(string: urlString){
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url){(data, response, error) in
+                if error != nil {
+                    return
+                }
+                if let safeData = data {
+                    print("Inside of performRequest ", safeData)
+                }
+            }
+            task.resume()
+        }
     }
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
